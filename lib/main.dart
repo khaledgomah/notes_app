9bc052e0498ac1d9d516c110616f2504a88dac45
khaled <khaledgomah10@gmail.com>
2023-08-27 13:46:56 +0300

    fix problem

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() async {
     await Hive.initFlutter();
   Bloc.observer = SimpleBlocObserver();  
   Hive.registerAdapter(NoteModelAdapter());
-  Hive.openBox<NoteModel>(kNotesBox);
+ await Hive.openBox<NoteModel>(kNotesBox);
   
   runApp(const NotesApp());
 }
